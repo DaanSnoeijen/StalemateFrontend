9309061fe6537a92a7b8551dfc8359a2cd100139
Daan Snoeijen <438919@student.fontys.nl>
2021-03-15 11:25:13 +0100

    Mainpage
    Hoofdpagina gemaakt met naam en achtergrondkleur

---
+++ src/MainPage.js
@@ -0,0 +1,12 @@
+import ChessBoard from './ChessBoard';
+
+const MainPage = () => {
+    return ( 
+        <div className="MainPage">
+            <h1>Stalemate</h1>
+            <ChessBoard />
+        </div>
+     );
+}
+ 
+export default MainPage;
