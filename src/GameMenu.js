013f4154a9ca27912e8fdb16f7f624918e7e3ce1
Daan Snoeijen <438919@student.fontys.nl>
2021-03-16 14:58:45 +0100

    Menu component toegevoegd
    Het werkt alleen nog niet

--- src/GameMenu.js
+++ src/GameMenu.js
@@ -1,11 +1,28 @@
 const GameMenu = () => {
 
-    const onMenuClick = () => {
+    const onMenuClickOpen = () => {
+        document.getElementById("sideBarClosed").style.position = "relative";
+        document.getElementById("sideBarOpened").style.position = "static";
+    }
 
+    const onMenuClickClose = () => {
+        document.getElementById("sideBarOpened").style.position = "relative";
+        document.getElementById("sideBarClosed").style.position = "static";
     }
 
     return ( 
-        <button>=</button>
+        <div>
+            <div id="sideBarOpened">
+                <button onClick={onMenuClickOpen} className="menu-button">=</button>
+            </div>
+            
+            <div id="sideBarClosed" className="sidebar">
+                <button onClick={onMenuClickClose}>=</button>
+
+                <input type="text" className="username"></input>
+                <input type="text" className="password"></input>
+            </div>
+        </div>
      );
 }
  
