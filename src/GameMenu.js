c20ddeb26c56011b7486d35c42518841d1863188
Daan Snoeijen <438919@student.fontys.nl>
2021-03-22 14:57:46 +0100

    Betere styling

--- src/GameMenu.js
+++ src/GameMenu.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const GameMenu = () => {
 
     const onMenuClickOpen = () => {
@@ -10,6 +12,8 @@ const GameMenu = () => {
         document.getElementById("sideBarClosed").style.position = "static";
     }
 
+    const [username, setUsername] = useState();
+
     return ( 
         <div>
             <div id="sideBarOpened">
@@ -17,10 +21,15 @@ const GameMenu = () => {
             </div>
             
             <div id="sideBarClosed" className="sidebar">
-                <button onClick={onMenuClickClose}>=</button>
-
-                <input type="text" className="username"></input>
-                <input type="text" className="password"></input>
+                <div className="button">
+                    <button onClick={onMenuClickClose}>=</button>
+                </div>
+                <div className="username">
+                    <input type="text" placeholder="Username"></input>
+                </div>
+                <div className="password">
+                    <input type="text" placeholder="Password"></input>
+                </div>
             </div>
         </div>
      );
